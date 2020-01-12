84d34e53847e5ef5f789ee5bbf8c9e50b200b609
jgorlin <32849866+jgorlin@users.noreply.github.com>
2020-01-12 15:27:26 -0700

    Update ExtDirectServiceProvider.php

--- src/Bulforce/ExtDirect/ExtDirectServiceProvider.php
+++ src/Bulforce/ExtDirect/ExtDirectServiceProvider.php
@@ -1,55 +1,41 @@
 <?php namespace Bulforce\ExtDirect;
-
 use Illuminate\Support\ServiceProvider;
-
-class ExtDirectServiceProvider extends ServiceProvider {
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-
-		$this->app['ext-direct'] = $this->app->share(function($app)
-        {
-            return new ExtDirect;
-        });
-
-        $this->app->booting(function()
-        {
+class ExtDirectServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('ext-direct', 'Bulforce\ExtDirect\ExtDirect');
+        $this->app->booting(function () {
             $loader = \Illuminate\Foundation\AliasLoader::getInstance();
             $loader->alias('ExtDirect', 'Bulforce\ExtDirect\Facades\ExtDirect');
         });
-
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('ext-direct');
-	}
-
+    }
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('ext-direct');
+    }
 }
